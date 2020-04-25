5058bf24fe9426f3a88a4c42f6a1cf9e7ec64f19
JanMouwes <jan.mouwes@gmail.com>
2020-04-26 00:34:42 +0200

    Made team randomisation far more random

--- src/randomise-teams.js
+++ src/randomise-teams.js
@@ -2,16 +2,42 @@
  * @param {number}amountOfTeams
  */
 export default function randomiseMembers(members, amountOfTeams) {
+
+    /**
+     * @param {number} max
+     * @returns {number}
+     */
+    const randomInt = (max) => {
+        return Math.round(Math.random() * max);
+    }
+
+    /**
+     *
+     * @param {*[]} array
+     * @returns {*[]}
+     */
     const randomiseArray = (array) => {
-        let returnArray = [];
-        for (const member of array) {
-            // 50% chance
-            if (Math.random() > .5) {
-                returnArray.push(member)
-            } else {
-                returnArray.unshift(member)
-            }
+        // Clone array
+        let returnArray = [...array];
+
+        /**
+         *
+         * @param {*[]} swapArray
+         * @returns {function(number, number)}
+         */
+        const createSwap = (swapArray) => (index1, index2) => {
+            const temp = swapArray[index1];
+            swapArray[index1] = swapArray[index2];
+            swapArray[index2] = temp;
         }
+
+        const swap = createSwap(returnArray);
+
+        // Swap each item in the array with a random other item
+        array.forEach((item, index) => {
+            const swapIndex = randomInt(array.length - 1);
+            swap(index, swapIndex);
+        });
         return returnArray;
     }
 
@@ -27,7 +53,9 @@ export default function randomiseMembers(members, amountOfTeams) {
     // Round-robin random member to team
     for (let i = 0; i < membersRandomOrder.length;) {
         for (const team of teams) {
-            if (i === membersRandomOrder.length) { break; }
+            if (i === membersRandomOrder.length) {
+                break;
+            }
 
             const randomMember = membersRandomOrder[i];
             team.push(randomMember);
